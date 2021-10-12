1ff93d022a0a38ac0e66ac92a10aacee49decac7
dronik300 <71210926+dronik300@users.noreply.github.com>
2021-10-12 19:28:17 +0300

    Add files via upload

---
+++ com2com(Lab_1)/com2com/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace lr_1
+{
+    partial class Com_port
+    {
+ 
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        private void InitializeComponent()
+        {
+            this.InputBox = new System.Windows.Forms.TextBox();
+            this.ComboBox = new System.Windows.Forms.ComboBox();
+            this.Debug = new System.Windows.Forms.Label();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.OutputBox = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // InputBox
+            // 
+            this.InputBox.Enabled = false;
+            this.InputBox.Location = new System.Drawing.Point(0, 21);
+            this.InputBox.Multiline = true;
+            this.InputBox.Name = "InputBox";
+            this.InputBox.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.InputBox.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.InputBox.Size = new System.Drawing.Size(341, 104);
+            this.InputBox.TabIndex = 0;
+            this.InputBox.WordWrap = false;
+            this.InputBox.TextChanged += new System.EventHandler(this.InputBox_TextChanged);
+            // 
+            // ComboBox
+            // 
+            this.ComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ComboBox.FormattingEnabled = true;
+            this.ComboBox.Location = new System.Drawing.Point(232, 394);
+            this.ComboBox.Name = "ComboBox";
+            this.ComboBox.Size = new System.Drawing.Size(121, 28);
+            this.ComboBox.TabIndex = 1;
+            this.ComboBox.SelectedIndexChanged += new System.EventHandler(this.ComboBox_SelectedIndexChanged_1);
+            // 
+            // Debug
+            // 
+            this.Debug.AutoSize = true;
+            this.Debug.Location = new System.Drawing.Point(6, 27);
+            this.Debug.Name = "Debug";
+            this.Debug.Size = new System.Drawing.Size(21, 20);
+            this.Debug.TabIndex = 2;
+            this.Debug.Text = "...";
+            // 
+            // SendButton
+            // 
+            this.SendButton.Location = new System.Drawing.Point(232, 365);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(121, 23);
+            this.SendButton.TabIndex = 3;
+            this.SendButton.Text = "Send";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // OutputBox
+            // 
+            this.OutputBox.ColumnWidth = 10;
+            this.OutputBox.FormattingEnabled = true;
+            this.OutputBox.HorizontalScrollbar = true;
+            this.OutputBox.ItemHeight = 20;
+            this.OutputBox.Location = new System.Drawing.Point(0, 21);
+            this.OutputBox.Name = "OutputBox";
+            this.OutputBox.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.OutputBox.Size = new System.Drawing.Size(341, 104);
+            this.OutputBox.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.InputBox);
+            this.groupBox1.Location = new System.Drawing.Point(12, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(341, 125);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Input";
+            this.groupBox1.AutoSizeChanged += new System.EventHandler(this.groupBox1_Enter);
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.OutputBox);
+            this.groupBox2.Location = new System.Drawing.Point(12, 138);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(341, 146);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Output";
+            this.groupBox2.AutoSizeChanged += new System.EventHandler(this.Form1_Load);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.Debug);
+            this.groupBox3.Location = new System.Drawing.Point(12, 290);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(341, 74);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Control/Debug";
+            this.groupBox3.AutoSizeChanged += new System.EventHandler(this.Form1_Load);
+            // 
+            // Com
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(365, 434);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.SendButton);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.ComboBox);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Com";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Com_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+                this.ResumeLayout(false);
+
+        }
+
+        
+
+        private System.Windows.Forms.Label Debug;
+        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.ComboBox ComboBox;
+        private System.Windows.Forms.ListBox OutputBox;
+        private System.Windows.Forms.TextBox InputBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
---
+++ com2com(Lab_1)/com2com/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+using System.Threading;
+
+namespace lr_1
+{
+    public partial class Com_port : Form
+    {
+        SerialPort ComPort;
+        Thread ReadThread;
+        String PortNumber;
+        private Staffing staffing;
+        int Change = 0;
+        static bool CanRead = false;
+        static string[] Ports = SerialPort.GetPortNames();
+        public Com_port()
+        {
+            InitializeComponent();
+            this.FormClosing += Com_FormClosing;
+            Debug.Text = "";                                            //Add ports in ComboBox
+            ComboBox.Items.Add("port not selected");
+            ComboBox.Items.AddRange(Ports);
+            ComboBox.SelectedItem = "port not selected";
+            Change--; //отвечает за изменение пользователем 1-пользователь, 0-изменение в коде
+            ReadThread = new Thread(read);
+            ReadThread.Start();
+        }
+
+        private void serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e) {        //if received messasge in port, signal to read it
+            Debug.Text = ("got message\n");
+            CanRead = true;
+        }
+
+        private void read() {                                        // Thread for check new message in port
+            while (true) {
+                if (CanRead) 
+                {
+                    try {
+                        OutputBox.Invoke((MethodInvoker)delegate 
+                        {
+                            string message;
+                            message = ComPort.ReadLine();
+                            Debug.Invoke((MethodInvoker)delegate
+                            {
+                                Debug.Text += message;
+                            });
+                            message = message.Replace("@+","g");
+                            OutputBox.Items.Add(message); 
+                        });
+                    }
+                    catch (TimeoutException e)      { Console.WriteLine("TimeoutException -... " + e.Message); }
+                    catch (ObjectDisposedException) { Console.WriteLine("ObjectDisposedException \n"); }
+                    finally { CanRead = false;
+                    }
+                }
+            }
+        }
+
+        //portName - хранит текущий порт, tmp_port - старый порт 
+        //PortName сохраняет имя нового порта, отключаемся от старого порта
+        //и пытемся подключится к новому. если не получается, то подключаемся к старому
+        private void ComboBox_SelectedIndexChanged_1(object sender, EventArgs e) {                      
+            Change++;
+            string PreviousName = PortNumber;
+            PortNumber = ComboBox.SelectedItem.ToString();
+            if (PortNumber == "port not selected")
+            {
+                InputBox.Enabled = false;
+            }
+
+            if (PortNumber != "port not selected")
+            {
+                InputBox.Enabled = true;
+                if (Change == 1)
+                {
+                    Change--;
+                    try
+                    {
+                        if (ComPort != null) { ComPort.Close(); }
+                        ComPort = new SerialPort(PortNumber);
+                        ComPort.Encoding = Encoding.Unicode;
+                        ComPort.Open();
+                        Debug.Text = "Select " + PortNumber;
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                        ComPort.Close();
+                        ComPort = new SerialPort(PreviousName);
+                        ComPort.Encoding = Encoding.Unicode;
+                        ComPort.Open();
+                        Debug.Text = "Com Port is Closed. \nConnect to " + PreviousName;
+                        Change--;
+                        ComboBox.SelectedItem = PreviousName;
+                    }
+                    catch (IOException) { Debug.Text = "The port response time has expired. \nConnect to " + PreviousName; Change--; ComboBox.SelectedItem = PreviousName; }
+                    finally { 
+                        ComPort.DataReceived += new SerialDataReceivedEventHandler(serialPort_DataReceived); }
+                }
+            }
+            if(PortNumber == "port not selected" && PreviousName != null) { ComPort.Close(); }
+        }
+
+        private void SendButton_Click(object sender, EventArgs e){
+            if (PortNumber != "port not selected")
+            {
+                try
+                {
+                    string writeLine = Convert.ToString(InputBox.Text);
+                    staffing = new Staffing();
+                    writeLine = writeLine.Replace("g", "@+");
+                    ComPort.WriteLine(writeLine);
+                    InputBox.Text = "";
+                    Debug.Text = "send message";
+                }
+                catch (TimeoutException) { }
+            }
+            else
+            {
+                Debug.Text="Select com port";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e){
+        }
+        
+        private void OutputBox_SelectedIndexChanged(object sender, EventArgs e){
+        }
+
+        private void comboBox_SelectedIndexChanged(object sender, EventArgs e){
+        }
+
+        public void Com_FormClosing(object sender, FormClosingEventArgs e){
+            ReadThread.Abort();
+            if(PortNumber != "port not selected")
+            {
+                ComPort.Close();
+            }
+            System.Environment.Exit(0);
+            System.Environment.FailFast("Exit Error!");
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InputBox_TextChanged(object sender, EventArgs e) {                 //Send message if click enter 
+            if (PortNumber == "port not selected")
+            {
+                InputBox.Enabled = true;
+            } 
+            
+            if (InputBox.Text.Length > 0 && PortNumber != "port not selected") {
+                if (InputBox.Text[InputBox.Text.Length - 1] == '\n' && InputBox.Text.TrimEnd('\r', '\n') != "") {
+                    try {
+                        string message = Convert.ToString(InputBox.Text.TrimEnd('\r', '\n'));
+                        message = message.Replace("g","@+");
+                        ComPort.WriteLine(message);
+                        InputBox.Text = null;
+                        Debug.Text = "send message";
+                    } catch (InvalidOperationException) { Debug.Text = "Port is Close. Select another port"; }
+                }
+            }
+        }
+    }
+}
